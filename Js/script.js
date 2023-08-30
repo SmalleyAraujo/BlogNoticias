f7b8d4840788df4b5902284043cf347d4afcfbfe
SmalleyAraujo <133250347+SmalleyAraujo@users.noreply.github.com>
2023-08-30 17:57:17 -0300

    Update

---
+++ Js/script.js
@@ -0,0 +1,42 @@
+const btnButtonMenu = $("#btnButtonMenu")
+
+const linkNav = $("#linkNav")  
+
+btnButtonMenu.on('click' , function(){
+    linkNav.slideToggle(2000)
+})
+
+
+
+// const mobile =document.getElementById("mobile")
+// const mobile =document.querySelector("#mobile")
+ const mobile = $("#mobile")
+ const desktop = $("#desktop")
+ const flowwork = $("#flowwork")
+
+ const descricaoMObile = $("#descricaoMObile")
+ const descricaoDescktop = $("#descricaoDescktop")
+ const descricaoFlowwork = $("#descricaoFlowwork")
+
+//  mobile.addEventlistener('click', mostrarDescMobile)
+
+//  function mostrarDescMobile(){
+
+//  }
+// function anonima
+mobile.on('click' , function(){
+    descricaoMObile.slideToggle(500)
+    descricaoDescktop.slideUp(100)
+    descricaoFlowwork.slideUp(100)
+})
+desktop.on('click' , function(){
+    descricaoDescktop.slideToggle(500)
+    descricaoFlowwork.slideUp(100)
+    descricaoMObile.slideUp(100)
+})
+flowwork.on('click' , function(){
+    descricaoFlowwork.slideToggle(500)
+    descricaoDescktop.slideUp(100)
+    descricaoMObile.slideUp(100)
+})
+
